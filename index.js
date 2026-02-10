c220eb30dc6c0a44ec58e52bf29ac94ba625eee3
lukekania <lukes.work@icloud.com>
2026-02-10 10:06:27 +0100

    refactor: convert generate-release-notes from commonjs to esm

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
-const fs = require("fs");
-const path = require("path");
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import fs from "fs";
+import path from "path";
 
 // -------- helpers --------
 
